10265dab855ecd2ce4cbf31e0ec920981a33be3c
greenchapeljohn <greenchapeljohn@gmail.com>
2023-03-28 08:17:36 +0100

    Add a start obd function in the api Readme updates

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # AutoCoder
 An open source in car head unit
 
+## Getting Started
+To start the app run
 
-## For OBD connection
+~~~ 
+go run main.go
+~~~
+
+To start the OBD connection send a POST request to
+~~~ 
+127.0.0.1:8080/start-obd
+~~~
+
+To get car data send a GET request to
+~~~
+127.0.0.1:8080/all-car-data
+~~~
+
+### For OBD connection
 This project uses a modified version of https://github.com/rzetterberg/elmobd
 As the library for talking to the ELM OBD device
 
 Can be tested by running elm emulator
 https://github.com/Ircama/ELM327-emulator
+~~~
 python -m elm -s car -n 35000 -l
-
+~~~
--- autocoder-api/main.go
+++ autocoder-api/main.go
@@ -21,20 +21,54 @@ type CarData struct {
 var obdDevice *elmobd.Device
 var rpm *elmobd.EngineRPM
 var carData CarData
+var obdRunning = false
+var addr *string
+var debug *bool
 
 // Create the API layer
-func createObdAPI() {
+func createAPI() {
 	r := mux.NewRouter()
 	// all data from the car
-	r.HandleFunc("/all-data", GetCarData).Methods("GET")
+	r.HandleFunc("/all-car-data", GetCarData).Methods("GET")
+	r.HandleFunc("/start-obd", PostStartOBD).Methods("POST")
 	http.ListenAndServe(":8080", r)
 }
 
 // Get all the data from the car
 func GetCarData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(carData)
+	if obdRunning == false {
+		// status not ok
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(carData)
+	}
+}
+
+// function with post request to start the obd
+func PostStartOBD(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if obdRunning == false {
+		// start the obd
+		obdRunning = true
+		// Create a new device
+		dev, err := elmobd.NewDevice(*addr, *debug)
+		if err != nil {
+			fmt.Println("error", err)
+			return
+		}
+		obdDevice = dev
+
+		// Create the API
+		// checkSupportedObdCommands()
+		go getFastData()
+		go getSlowData()
+		w.WriteHeader(http.StatusOK)
+	} else {
+		// obd already running
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 // Check the car has these commands
@@ -112,32 +146,19 @@ func GetCoolantTemp() string {
 // MAIN
 func main() {
 	// command line flags
-	addr := flag.String(
+	addr = flag.String(
 		"addr",
 		// "test:///dev/ttyUSB0",
 		"tcp://127.0.0.1:35000",
 		"Address of the ELM327 device to use (use either test://, tcp://ip:port or serial:///dev/ttyS0)",
 	)
-	debug := flag.Bool(
+	debug = flag.Bool(
 		"debug",
 		true,
 		"Enable debug outputs",
 	)
 	flag.Parse()
 
-	// Create a new device
-	dev, err := elmobd.NewDevice(*addr, *debug)
-	if err != nil {
-		fmt.Println("error", err)
-		return
-	}
-	obdDevice = dev
-
-	// Create the API
-	// checkSupportedObdCommands()
-	go getFastData()
-	go getSlowData()
-
 	// start the API layer
-	createObdAPI()
+	createAPI()
 }
