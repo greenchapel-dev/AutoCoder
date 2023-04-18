66a173dd78aa935e86016d9174a6e4f2a6c77754
greenchapeljohn <greenchapeljohn@gmail.com>
2023-04-18 14:56:26 +0100

    combine into 1 file

--- autocoder-api/main.go
+++ autocoder-api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/greenchapel-dev/elmobd" // modified from https://github.com/rzetterberg/elmobd
@@ -76,7 +77,8 @@ func main() {
 	addr = flag.String(
 		"addr",
 		// "test:///dev/ttyUSB0",
-		"tcp://127.0.0.1:35000",
+		// "tcp://127.0.0.1:35000",
+		"serial:///dev/ttyS3",
 		"Address of the ELM327 device to use (use either test://, tcp://ip:port or serial:///dev/ttyS0)",
 	)
 	debug = flag.Bool(
@@ -88,4 +90,77 @@ func main() {
 
 	// start the API layer
 	createAPI()
+}
+
+// / move to own file
+// Check the car has these commands
+func checkSupportedObdCommands() {
+	supported, err := obdDevice.CheckSupportedCommands()
+
+	if err != nil {
+		fmt.Println("Failed to check supported commands", err)
+		return
+	}
+
+	rpm = elmobd.NewEngineRPM()
+
+	if supported.IsSupported(rpm) {
+		fmt.Println("The car supports checking RPM")
+	} else {
+		fmt.Println("The car does NOT supports checking RPM")
+	}
+}
+
+// Thread for getting the fast data from the car on a regular interval
+func getFastData() {
+	// create a ticker for every 100ms
+	ticker := time.NewTicker(100 * time.Millisecond)
+	// run forever
+	for {
+		select {
+		case <-ticker.C:
+			// get the rpm
+			GetRPM()
+		}
+	}
+}
+
+// Thread for getting the slow data from the car on a regular interval
+func getSlowData() {
+	// create a ticker for every 1000ms
+	ticker := time.NewTicker(1000 * time.Millisecond)
+	// run forever
+	for {
+		select {
+		case <-ticker.C:
+			// get the rpm
+			GetCoolantTemp()
+		}
+	}
+}
+
+// Get RPM
+func GetRPM() string {
+	rpm = elmobd.NewEngineRPM()
+	rmpRes, err := obdDevice.RunOBDCommand(rpm)
+	if err != nil {
+		fmt.Println("error", err)
+		return "error: " + err.Error()
+	}
+	rpmVal := rmpRes.ValueAsLit()
+	carData.RPM = rpmVal
+	return rpmVal
+}
+
+// Get Engine coolent temp
+func GetCoolantTemp() string {
+	coolantTemp := elmobd.NewCoolantTemperature()
+	coolantTempRes, err := obdDevice.RunOBDCommand(coolantTemp)
+	if err != nil {
+		fmt.Println("error", err)
+		return "error: " + err.Error()
+	}
+	coolantTempVal := coolantTempRes.ValueAsLit()
+	carData.CoolantTemp = coolantTempVal
+	return coolantTempVal
 }
